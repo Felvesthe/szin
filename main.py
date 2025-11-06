163378ce707410a0b6762c40cf08538b72557dda
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2025-11-06 09:53:39 +0100

    Remove DfsAlgorithm class
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from BfsAlgorithm import BfsAlgorithm
-from DfsAlgorithm import DfsAlgorithm
 from Labyrinth import Labyrinth
 from theseus import Theseus
 
 labirynt1 = Labyrinth('data/5x5.txt')
 
 bfs_algorithm = BfsAlgorithm()
-dfs_algorithm = DfsAlgorithm()
 
 #theseus = Theseus(labirynt1.matrix, labirynt1.n, labirynt1.m)
 #theseus.solve(0,0)
