3d957d877e96bdb8984179dca6e4c0686ebe8007
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2025-11-06 22:09:54 +0100

    BfsAlgorithm: Code cleanup
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- BfsAlgorithm.py
+++ BfsAlgorithm.py
@@ -12,27 +12,24 @@ class BfsAlgorithm:
     def solve(self, heuristic = False):
         # pomiar czasu i pamięci: początek
         logger = self.create_logger_instance(heuristic)
-
         logger.start_measure()
-
-        max_queue_size = 1 # maksymalna liczba elementów w kolejce (szerokość drzewa)
-        total_visited = 0 # liczba odwiedzonych węzłów
 
         # algorytm
         start = Position(0, 0)
         finish = Position(self.labyrinth.n - 1, self.labyrinth.m - 1)
 
-        queue = []
+        total_visited_nodes = 0
+
         if not heuristic:
             queue = deque([start]) # FIFO
         else:
+            queue = []
             counter = 0
             heappush(queue, (0, counter, start))
         visited = { Position(0, 0): None } # Position(nowe_pole): Position(pole_z_którego_tutaj_przyszliśmy)
                                            # Domyślnie ustawione na start,
                                            # aby program nie wpadał w nieskończoną pętlę
-        result = [[0 if self.labyrinth.matrix[i][j] == 0 else 1
-                   for j in range(self.labyrinth.m)] for i in range(self.labyrinth.n)] # kopia labiryntu z zachowaniem układu
+        result = [row[:] for row in self.labyrinth.matrix]
 
         # dopóki kolejka nie jest pusta,
         # to pętla pobiera pierwszy element z kolejki i oznacza go jako odwiedzony w tablicy wynikowej result
@@ -41,7 +38,7 @@ class BfsAlgorithm:
                 current_pos = queue.popleft()
             else:
                 _, _, current_pos = heappop(queue)
-            total_visited += 1
+            total_visited_nodes += 1
             result[current_pos.x][current_pos.y] = 2 # oznaczenie jako odwiedzone
 
             if current_pos == finish:
@@ -60,10 +57,6 @@ class BfsAlgorithm:
                         counter += 1
                         heappush(queue, (h, counter, neighbor))
 
-            # aktualizacja maksymalnej liczby węzłów w kolejce
-            if len(queue) > max_queue_size:
-                max_queue_size = len(queue)
-
         # sprawdzamy, czy punkt końcowy był odwiedzony, jeśli tak, to jest zapisywana ścieżka powrotna do result,
         # na podstawie danych zapisanych w visited
         # ścieżka jest oznaczana cyfrą 3
@@ -76,14 +69,12 @@ class BfsAlgorithm:
             path = []
 
         # pomiar czasu i pamięci: koniec
-        execution_time, peak_memory_kb = logger.stop_measure()
+        logger.stop_measure()
 
         logger.save(
             result,
             path,
-            execution_time,
-            peak_memory_kb,
-            total_visited
+            total_visited_nodes
         )
 
         return result
